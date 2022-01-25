1d0f0ee7a5f9b4d4365c02e567b7a4dfeef1a3ef
mahendradiaz <diazmahendra29.dm@gmail.com>
2022-01-25 17:18:35 +0700

    ada js yg di hilangkan

--- JS/scrollnav.js
+++
@@ -1,9 +0,0 @@
-$(window).scroll(function(){
-    if($(window).scrollTop()){
-        $(".nav-header").addClass("grey");
-    }
-    else{
-        $(".nav-header").removeClass("grey");
-
-    }
-});
